d730c664389f643f830a7b1f486e1846309d6077
admin <sevandwik@gmail.com>
2025-08-06 14:12:00 +0700

    perbaiki lagi2 view/index.php

--- app/Views/index.php
+++ app/Views/index.php
@@ -6,9 +6,7 @@
   <link rel="icon" href="<?= base_url('assets/business.png') ?>" type="image/png" sizes="16x16">
   <title><?= $title ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Bootstrap 5 CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
+  
   <!-- DataTables Bootstrap 5 CSS -->
   <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
 
