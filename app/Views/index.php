9afebb7f4917aa8bf2ab1207d5386e2168662939
admin <sevandwik@gmail.com>
2025-08-31 10:23:50 +0700

    update view lagi buat coba8

--- app/Views/index.php
+++ app/Views/index.php
@@ -73,7 +73,7 @@
                         <td class="border px-4 py-2">
                              
                                  
-                                  <?= $habis ?> -- <?= date('d-m-Y H:i:s',$sekarang) ?>
+                                  <?= $habis ?> -- <?= date('d-m-Y H:i:s',$sekarang) -- <?= date('d-m-Y H:i:s',$selisihDetik) ?>
                                   
                                 
                         </td>
