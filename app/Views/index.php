4c665de76177a8462f9f9e3e5d46a241f3a9d3f4
admin <sevandwik@gmail.com>
2025-09-12 14:10:07 +0700

    penambahan chart pembaruan2

--- app/Views/index.php
+++ app/Views/index.php
@@ -154,5 +154,9 @@
     });
 </script>
 </div>
+
+<div class="container copyright text-center mt-4">
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">Diskominfo Cilacap</strong> <span>All Rights Reserved</span></p>
+</div> 
 <?= $this->include('layout/footer') ?>
 
